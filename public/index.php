afdc8f72bb096527af3ab65b0fc6c5f4c8f7fc12
Vladislav <oxxxyKPSS@gmail.ru>
2019-04-22 18:16:59 +0300

    аутентификация

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
-require 'diplo.me/include.php';
-echo('it works!');
-?>
+require 'E:\servak\OSPanel\domains\diplo.me\include.php';?>
+<form action="login.php" method="POST">
+    <strong>Логин</strong>
+    <input type="text" name="login" value="<?php echo @$data['login']; ?>"><br>
+    <strong>Корпоративная почта</strong>
+    <input type="text" name="login" value="<?php echo @$data['login']; ?>"><br>
+    <strong>Пароль</strong>
+    <input type="password" name="password" value="<?php echo @$data['password']; ?>"><br>
+    <button type="submit" name="do_login">Войти</button>
+</form>
