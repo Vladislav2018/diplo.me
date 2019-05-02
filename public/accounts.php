255df485609278516aa49eb86f2ec4d092b7becd
Vladislav <oxxxyKPSS@gmail.ru>
2019-05-02 14:07:01 +0300

    оказывается, нужно всё делать анти-паттернами, тогда всё работает

--- public/accounts.php
+++ public/accounts.php
@@ -1,55 +1,30 @@
 <?php
    require '../include.php';//file with coonection to RedBean
    include_once '../helper.php';
-      function getEmployee()
+      function redirectUser()
       {
          $employee = R::FindOne('employees', 'WHERE user_id = ?', array($_SESSION['user_id']));
          b_dump($employee);
-         return $employee;
-      }
-      function getVerify()
-      {
-         if(empty(getEmployee()))
+         if(!empty($employee))
          {
-            return false;
-         }
-         else
-         return true;  
-      }
-      function getUserRole($c_user_role)
-      {
-         if(getVerify(true))
-         {
-            $c_user_role = getEmployee()['roles'];
-            return $c_user_role;
-         }   
-      }
-      function redirectUser()
-      {
-         if(getVerify(false))
-         {
-            ?><script type="text/javascript">
-            location = 'confirm.php';
-            </script><?php
-            exit();
-         }
-         else
-         {
-            if(getUserRole('worker'))
+            $c_user_role = $employee['roles'];
+            $_SESSION['role'] = $c_user_role;
+            b_dump($c_user_role);
+            if($c_user_role['roles'] == 'worker')
             {
                ?><script type="text/javascript">
                location = 'accTypes/worker.php';
                </script><?php
                exit();
             }
-            elseif(getUserRole('manager'))
+            elseif($c_user_role['roles'] == 'manager')
             {
                ?><script type="text/javascript">
                location = 'accTypes/worker.php';
                </script><?php
                exit();
             }
-            elseif(getUserRole('admin'))
+            elseif($c_user_role['roles'] == 'admin')
             {
                ?><script type="text/javascript">
                location = 'accTypes/admin.php';
@@ -57,8 +32,16 @@
                exit();
             }
          }
+         else
+         {
+            ?><script type="text/javascript">
+            location = 'confirm.php';
+            </script><?php
+            exit();
+         }
       }     
    b_dump($_SESSION);
+   redirectUser();
    ?>
 
 
